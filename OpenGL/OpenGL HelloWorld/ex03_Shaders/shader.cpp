a9a8a5832d464df18daf979d14e88c03724938e3
helyousfi <visioncomputer200@gmail.com>
2026-02-28 21:13:25 +0100

    Shaders

--- OpenGL/OpenGL HelloWorld/ex03_Shaders/shader.cpp
+++ OpenGL/OpenGL HelloWorld/ex03_Shaders/shader.cpp
@@ -34,9 +34,20 @@ Shader::Shader(const char* vertexPath, const char* fragmentPath) {
     glDeleteShader(vert);
     glDeleteShader(frag);
 }
-
+void Shader::setBool(const std::string& name, bool value) const
+{
+    glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+}
+void Shader::setInt(const std::string& name, int value) const
+{
+    glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+}
 void Shader::setFloat(const std::string& name, float value) const {
     glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+}
+void Shader::setVec2(const std::string& name, const glm::vec2& v) const
+{
+    glUniform2fv(glGetUniformLocation(ID, name.c_str()), 1, glm::value_ptr(v));
 }
 void Shader::setVec3(const std::string& name, const glm::vec3& v) const {
     glUniform3fv(glGetUniformLocation(ID, name.c_str()), 1, glm::value_ptr(v));
